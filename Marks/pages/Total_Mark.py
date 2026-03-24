d936e8e5b3f7c845db176513d03b12da67dfb648
Prabbie9 <prabaryan@outlook.com>
2026-03-24 22:41:15 +0000

    Delete Marks directory

--- Marks/pages/Total_Mark.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.write("No point calculating this now")
-
-st.divider()
-
-if st.button("Year 1 Modules", use_container_width=True):
-    st.switch_page("Year_1_Modules.py")
